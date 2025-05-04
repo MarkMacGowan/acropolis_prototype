7ea29f9f426b33afacb014ad3e0f7b6ad563cc19
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-05-04 22:22:55 +0100

    test: implementing-whirlwind-event

--- Assets/overcastHandler.cs
+++ Assets/overcastHandler.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class environmentHandler : MonoBehaviour
+public class overcastHandler : MonoBehaviour
 {
-    public List<GameObject> environmentEvent = new List<GameObject>();
+    [SerializeField] private GameObject cloud_object;
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(environmentEvent[0]);
+        
     }
 
     // Update is called once per frame
--- Assets/sandStormBehavior.cs
+++ Assets/sandStormBehavior.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class environmentHandler : MonoBehaviour
+public class sandStormBehavior : MonoBehaviour
 {
-    public List<GameObject> environmentEvent = new List<GameObject>();
+    //private Vector3 
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(environmentEvent[0]);
+        
     }
 
     // Update is called once per frame
